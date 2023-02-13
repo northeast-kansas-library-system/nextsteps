dbfdd7b65fd7682c0670b59731f61dcfbae47374
George Williams <will1410@users.noreply.github.com>
2023-02-12 19:24:29 -0600

    full update with new page for password changeover

---
+++ source/usergroup/ug.20230214.rs
@@ -0,0 +1,36 @@
+.. include:: /include.rst
+
+2023.02.14 - Agenda and notes
+=============================
+
+2023.02.14 - (Tuesday) 10:00 a.m. - 12:00 p.m. - NEKLS / online |br|
+`Zoom link <https://kslib.zoom.us/j/98749870131?pwd=bEczT0FlMUFTa1Z3a25XVkhGNlhlUT09>`_ |br|
+Zoom passcode 7858384090 |br|
+No recording of this meeting will be made |br|
+
+1. Announcements, news, brief items, etc.
+
+    - Introductions
+    - Additions to the agenda
+
+#. Changes to statistical data collection for 2023
+
+#. Password changeover
+
+  - More information - see ``
+
+#. Next meeting
+
+    - Meeting at Osage City Public Library: April 18 (Tuesday) 10:00 a.m. - 12:00 p.m.
+
+.. |ss| raw:: html
+
+    <strike>
+
+.. |se| raw:: html
+
+    </strike>
+
+.. |br| raw:: html
+
+    <br />
